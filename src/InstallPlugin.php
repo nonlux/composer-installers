ca5ee238e6fee08efa0790b251f1980a746e81f8
nonlux <nonluxi@gmail.com>
2015-10-19 20:01:50 +0300

    fix typo

--- src/InstallPlugin.php
+++ src/InstallPlugin.php
@@ -6,7 +6,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class InstallerPlugin implements PluginInterface
+class InstallPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
